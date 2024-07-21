7ab2a341988116d4dcca8a9d980fb9644c64461f
Karthik Harihara <karthikharihara@Karthiks-Air.home>
2024-07-21 16:09:23 -0700

    removing passwords

---
+++ resources/sendemail.py
@@ -0,0 +1,28 @@
+import smtplib
+import base64
+from email.mime.text import MIMEText
+from email.message import EmailMessage
+
+def sendEmail(mildex, date):
+    from_email = ""
+    from_password = ""
+    to_email = "hmahesh2@gmail.com"
+
+    subject = " Mildex for " + date
+    message = date + " Mildew Risk Index is " + str(mildex)
+
+    msg = EmailMessage()
+    msg['Subject'] = subject
+    msg['To'] = to_email
+    msg['From'] = from_email
+    msg.set_content(message)
+
+    try:
+        gmail = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        gmail.ehlo()
+        gmail.login(from_email, from_password)
+        gmail.send_message(msg)
+        gmail.close()
+        print("successfully send mail")
+    except:
+        print ("failed to send mail")
